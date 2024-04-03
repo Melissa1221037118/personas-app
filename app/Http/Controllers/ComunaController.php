760b8c56d00400453c0d5f804c866357b9f3cb3c
Melissa <melissavallecilla0821@gmail.com>
2024-04-02 23:42:31 -0500

    ajuste listado de comuna

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comuna;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class ComunaController extends Controller
 {
     /**
